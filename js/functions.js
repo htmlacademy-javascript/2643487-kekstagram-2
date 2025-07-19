826c53e725ad9555adaa7e35c92e63a755f8903e
Denis-Gorbunoff <volo_non_valeo@mail.ru>
2025-07-19 12:15:50 +0300

    Выполнит задания по пунктам 5.15 и 5.16

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,28 @@
-const checkStringLength = (str, maxLength) => str.length <= maxLength;
+// const checkStringLength = (str, maxLength) => str.length <= maxLength;
 
-const isPalindrome = (str) => {
-  const cleanedStr = str.replace(/\s/g, '').toLowerCase(); //Удаляет все пробелы из строки и понижает регистр
-  return cleanedStr === cleanedStr.split('').reverse().join(''); //разбивает строку на массив, собирает в обратном порядке и сравниает
+// const isPalindrome = (str) => {
+//   const cleanedStr = str.replace(/\s/g, '').toLowerCase(); //Удаляет все пробелы из строки и понижает регистр
+//   return cleanedStr === cleanedStr.split('').reverse().join(''); //разбивает строку на массив, собирает в обратном порядке и сравниает
+// };
+
+// const extractNumber = (input) => {
+//   const str = String(input); // Преобразует входное значение в строку
+//   const digits = str.replace(/\D/g, ''); // Удаляет всё, кроме цифр
+//   return parseInt(digits, 10); // Возвращает число или NaN
+// };
+
+const parseTime = (timeStr) => {
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return hours * 60 + minutes;
 };
 
-const extractNumber = (input) => {
-  const str = String(input); // Преобразует входное значение в строку
-  const digits = str.replace(/\D/g, ''); // Удаляет всё, кроме цифр
-  return parseInt(digits, 10); // Возвращает число или NaN
+const isMeetingWithinWorkday = (workStart, workEnd, meetingStart, duration) => {
+  const workStartMinutes = parseTime(workStart);
+  const workEndMinutes = parseTime(workEnd);
+  const meetingStartMinutes = parseTime(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + duration;
+
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
 };
 
-export { checkStringLength, isPalindrome, extractNumber };
+export { isMeetingWithinWorkday };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 import { createPhotosArray } from './mock-generator.js';
+import { isMeetingWithinWorkday } from './functions.js';
 
+window.console.log('ВЫВОД МОКОВЫХ ДАННЫХ');
 window.console.log(createPhotosArray());
 
+window.console.log('\nДЕМОНСТРАЦИЯ РАБОТЫ ФУНКЦИИ ОПРЕДЕЛЕНИЯ РАБОЧЕГО ВРЕМЕНИ');
+window.console.log(isMeetingWithinWorkday('08:00', '17:30', '14:00', 90)); // true
+window.console.log(isMeetingWithinWorkday('8:0', '10:0', '8:0', 120)); // true
+window.console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90)); // false
+window.console.log(isMeetingWithinWorkday('14:00', '17:30', '08:0', 90)); // false
+window.console.log(isMeetingWithinWorkday('8:00', '17:30', '08:00', 900)); // false
