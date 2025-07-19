9dbd8ea758be8d5f5938638a3e15051f8cc52fa2
Denis-Gorbunoff <volo_non_valeo@mail.ru>
2025-07-19 12:24:08 +0300

    init commit

--- js/main.js
+++ js/main.js
@@ -10,3 +10,5 @@ window.console.log(isMeetingWithinWorkday('8:0', '10:0', '8:0', 120)); // true
 window.console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90)); // false
 window.console.log(isMeetingWithinWorkday('14:00', '17:30', '08:0', 90)); // false
 window.console.log(isMeetingWithinWorkday('8:00', '17:30', '08:00', 900)); // false
+
+// Комметарий для коммита
