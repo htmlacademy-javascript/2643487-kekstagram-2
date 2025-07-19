00b4f1044d399f4df49b28a7c0663451df05c816
Denis-Gorbunoff <volo_non_valeo@mail.ru>
2025-07-19 22:12:50 +0300

    init commit

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,3 @@
-// const checkStringLength = (str, maxLength) => str.length <= maxLength;
-
-// const isPalindrome = (str) => {
-//   const cleanedStr = str.replace(/\s/g, '').toLowerCase(); //Удаляет все пробелы из строки и понижает регистр
-//   return cleanedStr === cleanedStr.split('').reverse().join(''); //разбивает строку на массив, собирает в обратном порядке и сравниает
-// };
-
-// const extractNumber = (input) => {
-//   const str = String(input); // Преобразует входное значение в строку
-//   const digits = str.replace(/\D/g, ''); // Удаляет всё, кроме цифр
-//   return parseInt(digits, 10); // Возвращает число или NaN
-// };
-
 const parseTime = (timeStr) => {
   const [hours, minutes] = timeStr.split(':').map(Number);
   return hours * 60 + minutes;
