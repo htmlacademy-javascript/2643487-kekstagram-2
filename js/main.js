dedef3dd41065131f55cf3080bbafe4a121085d5
Denis-Gorbunoff <volo_non_valeo@mail.ru>
2025-08-11 17:26:23 +0300

    Добавит модуль для работы с сервером через Fetch Api

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-import { createPhotosArray } from './mock-generator.js';
+//import { createPhotosArray } from './mock-generator.js';
+import { getData } from './api.js';
 import { initGallery } from './init-gallery.js';
 import { initImageUploadForm } from './image-upload-form.js';
 
-initGallery(createPhotosArray());
+const initApp = async () => {
+  try {
+    const photos = await getData();
+    initGallery(photos);
+  } catch {
+    window.console.log('error');
+    //showDataError();
+  }
+};
+
+initApp();
+
+//initGallery(createPhotosArray());
 initImageUploadForm();
 
